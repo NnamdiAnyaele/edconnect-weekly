20b85f84672dc3f9d9cedee71d7d8d18a316fedd
NnamdiAnyaele <nnamdianya2011@gmail.com>
2021-07-08 22:59:34 +0100

    Completed Week 6 Exercise

---
+++ app/server/models/data_model.js
@@ -0,0 +1,55 @@
+class DataModel {
+    constructor() {
+        this.data = [];
+    }
+
+    getAll() {
+        return this.data;
+    }
+
+    getById(id) {
+    let user = this.data.find(item => item.id == id);
+      if (!user) {
+        return null
+      }
+    return user;
+    }
+
+    save(obj) {
+        if (this.validate(obj)) {
+            this.data.push(obj);
+            return true;
+        }
+        return false;
+    }
+
+    update(obj, id) {
+      let user = this.data.find(item => item.id == id);
+      if (!user) {
+        return false
+      }
+      for (let prop in user) {
+        user[prop] = obj[prop];
+      }
+      return true
+    }
+
+    delete(id) {
+    let user = this.data.find(item => item.id == id);
+    if (!user) {
+      return false
+    }
+    let index = this.data.indexOf(user);
+     this.data.splice(index, 1)
+    return true
+    }
+
+    // this method will be overriden in the sub classes
+    validate(obj) {
+        return false;
+    }
+}
+
+// Do not worry about the below for now; It is included so that we can test your code
+// We will cover module exports in later parts of this course
+module.exports = DataModel;
---
+++ app/server/models/users.js
@@ -0,0 +1,63 @@
+const DataModel = require('./data_model');
+
+class User {
+    constructor(id, firstname, lastname, email, password, matricNumber, program, graduationYear) {
+      this.id = id;
+      this.firstname = firstname;
+      this.lastname = lastname;
+      this.email = email;
+      this.password = password;
+      this.matricNumber = matricNumber;
+      this.program = program;
+      this.graduationYear = graduationYear;
+    }
+
+    getFullName() {
+      return `${this.firstname} ${this.lastname}`
+
+    }
+}
+
+class Users extends DataModel {
+    authenticate(email, password) {
+    let user = this.data.find(item => item.email == email && item.password == password);
+    if (user) {
+      return true
+    }
+    return false
+    }
+
+    getByEmail(email) {
+    let user = this.data.find(item => item.email == email)
+    if (user) {
+      return user
+    }
+    return null;
+    }
+
+    getByMatricNumber(matricNumber) {
+    let user = this.data.find(item => item.matricNumber == matricNumber)
+    if (user) {
+      return user
+    }
+    return null;
+    }
+
+    validate(obj) {
+    let emptyProp = Object.values(obj).some(x => (x == null || x == ''));
+    let userByEmail = this.data.some(myObj => myObj.email == obj.email)
+    let userByMatric = this.data.some(myObj => myObj.matricNumber == obj.matricNumber)
+    if(emptyProp || userByEmail || userByMatric || obj.password.length < 7) {
+      return false
+    } else {
+        return true
+      }
+    }
+}
+
+// Do not worry about the below for now; It is included so that we can test your code
+// We will cover module exports in later parts of this course
+module.exports = {
+    User,
+    Users
+};
