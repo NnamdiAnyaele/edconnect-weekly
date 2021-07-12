b84e34a7872a612fb69b69e764c059c8946097eb
NnamdiAnyaele <nnamdianya2011@gmail.com>
2021-07-12 10:36:23 +0100

    week 7 project

---
+++ app/web/src/shared/Layout.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header"
+import Footer from "./Footer"
+
+const Layout = props => {
+  return (
+    <>
+      <Header />
+      <main className="mx-auto">{props.children}</main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
