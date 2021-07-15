8868dac5fc91cba54430425a95a96456c499714c
NnamdiAnyaele <nnamdianya2011@gmail.com>
2021-07-15 15:59:52 +0100

    completed week 7 task

--- app/web/src/CreateProject.jsx
+++ app/web/src/CreateProject.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import { Alert, Button, Col, Form, Row } from "react-bootstrap"
 import { useHistory } from "react-router-dom"
 import Layout from "./shared/Layout"
